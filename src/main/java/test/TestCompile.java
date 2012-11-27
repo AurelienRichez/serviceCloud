aa1455aa7177492185716c3eae37d04488fb9f5a
aurelien <aurelien.richez@gmail.com>
2012-11-27 20:41:52 +0100

    Commit from JBoss Tools

--- src/main/java/test/TestCompile.java
+++ src/main/java/test/TestCompile.java
@@ -1,6 +1,5 @@
 package test;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,66 +24,65 @@ import compilation.CharSequenceJavaFileObject;
 @WebServlet("/TestCompile")
 public class TestCompile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestCompile() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public TestCompile() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// Full name of the class that will be compiled.
-        // If class should be in some package,
-        // fullName should contain it too
-        // (ex. "testpackage.DynaClass")
-        String fullName = "DynaClass";
+		// If class should be in some package,
+		// fullName should contain it too
+		// (ex. "testpackage.DynaClass")
+		String fullName = "DynaClass";
 
-        // Here we specify the source code of the class to be compiled
-        StringBuilder src = new StringBuilder();
-        src.append("public class DynaClass {\n");
-        src.append("    public String toString() {\n");
-        src.append("        return \"Hello, I am \" + ");
-        src.append("this.getClass().getSimpleName();\n");
-        src.append("    }\n");
-        src.append("}\n");
+		// Here we specify the source code of the class to be compiled
+		StringBuilder src = new StringBuilder();
+		src.append("public class DynaClass {\n");
+		src.append("    public String toString() {\n");
+		src.append("        return \"Hello, I am \" + ");
+		src.append("this.getClass().getSimpleName();\n");
+		src.append("    }\n");
+		src.append("}\n");
 
-        
-        // We get an instance of JavaCompiler. Then
-        // we create a file manager
-        // (our custom implementation of it)
-        JavaCompiler compiler = new EclipseCompiler();
-        JavaFileManager fileManager = new
-            compilation.ClassFileManager(compiler
-                .getStandardFileManager(null, null, null));
-        
+		// We get an instance of JavaCompiler. Then
+		// we create a file manager
+		// (our custom implementation of it)
+		JavaCompiler compiler = new EclipseCompiler();
+		JavaFileManager fileManager = new compilation.ClassFileManager(
+				compiler.getStandardFileManager(null, null, null));
 
-        // Dynamic compiling requires specifying
-        // a list of "files" to compile. In our case
-        // this is a list containing one "file" which is in our case
-        // our own implementation (see details below)
-        List<JavaFileObject> jfiles = new ArrayList<JavaFileObject>();
-        jfiles.add(new CharSequenceJavaFileObject(fullName, src));
+		// Dynamic compiling requires specifying
+		// a list of "files" to compile. In our case
+		// this is a list containing one "file" which is in our case
+		// our own implementation (see details below)
+		List<JavaFileObject> jfiles = new ArrayList<JavaFileObject>();
+		jfiles.add(new CharSequenceJavaFileObject(fullName, src));
 
-        // We specify a task to the compiler. Compiler should use our file
-        // manager and our list of "files".
-        // Then we run the compilation with call()
-        
-        try {
-			compiler.getTask(null, fileManager, null, null,
-			    null, jfiles).call();
+		// We specify a task to the compiler. Compiler should use our file
+		// manager and our list of "files".
+		// Then we run the compilation with call()
 
-        // Creating an instance of our compiled class and
-        // running its toString() method
-		ClassLoader classLoader = fileManager.getClassLoader(null);
-		
-        Object instance = classLoader.loadClass(fullName).newInstance();
-        response.getWriter().append(instance.toString());
-        //response.getWriter().append(instance.toString());
+		try {
+			compiler.getTask(null, fileManager, null, null, null, jfiles)
+					.call();
+
+			// Creating an instance of our compiled class and
+			// running its toString() method
+			ClassLoader classLoader = fileManager.getClassLoader(null);
+
+			Object instance = classLoader.loadClass(fullName).newInstance();
+			response.getWriter().append(instance.toString());
+			// response.getWriter().append(instance.toString());
 		} catch (RuntimeErrorException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -101,54 +99,51 @@ public class TestCompile extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// Full name of the class that will be compiled.
-        // If class should be in some package,
-        // fullName should contain it too
-        // (ex. "testpackage.DynaClass")
-        String fullName = request.getParameter("fullName");
+		// If class should be in some package,
+		// fullName should contain it too
+		// (ex. "testpackage.DynaClass")
+		String fullName = request.getParameter("fullName");
 
-        // Here we specify the source code of the class to be compiled
-        String src = request.getParameter("source");
-        
-        response.getWriter().append(src + "\n");
-        
-        // We get an instance of JavaCompiler. Then
-        // we create a file manager
-        // (our custom implementation of it)
-        JavaCompiler compiler = new EclipseCompiler();
-        JavaFileManager fileManager = new
-            compilation.ClassFileManager(compiler
-                .getStandardFileManager(null, null, null));
-        
+		// Here we specify the source code of the class to be compiled
+		String src = request.getParameter("source");
 
-        // Dynamic compiling requires specifying
-        // a list of "files" to compile. In our case
-        // this is a list containing one "file" which is in our case
-        // our own implementation (see details below)
-        List<JavaFileObject> jfiles = new ArrayList<JavaFileObject>();
-        jfiles.add(new CharSequenceJavaFileObject(fullName, src));
+		response.getWriter().append(src + "\n");
 
-        // We specify a task to the compiler. Compiler should use our file
-        // manager and our list of "files".
-        // Then we run the compilation with call()
-        
-        try {
-			try {
-				compiler.getTask(null, fileManager, null, null, null, jfiles)
-						.call();
-			} catch (RuntimeException e) {
-				response.getWriter().append(e.getMessage());
-			}
-		// Creating an instance of our compiled class and
-        // running its toString() method
-		ClassLoader classLoader = fileManager.getClassLoader(null);
-		
-        Object instance = classLoader.loadClass(fullName).newInstance();
-        response.getWriter().append(instance.toString());
-        //response.getWriter().append(instance.toString());
+		// We get an instance of JavaCompiler. Then
+		// we create a file manager
+		// (our custom implementation of it)
+		JavaCompiler compiler = new EclipseCompiler();
+		JavaFileManager fileManager = new compilation.ClassFileManager(
+				compiler.getStandardFileManager(null, null, null));
+
+		// Dynamic compiling requires specifying
+		// a list of "files" to compile. In our case
+		// this is a list containing one "file" which is in our case
+		// our own implementation (see details below)
+		List<JavaFileObject> jfiles = new ArrayList<JavaFileObject>();
+		jfiles.add(new CharSequenceJavaFileObject(fullName, src));
+
+		// We specify a task to the compiler. Compiler should use our file
+		// manager and our list of "files".
+		// Then we run the compilation with call()
+
+		try {
+			compiler.getTask(null, fileManager, null, null, null, jfiles)
+					.call();
+
+			// Creating an instance of our compiled class and
+			// running its toString() method
+			ClassLoader classLoader = fileManager.getClassLoader(null);
+
+			Object instance = classLoader.loadClass(fullName).newInstance();
+			response.getWriter().append(instance.toString());
+			// response.getWriter().append(instance.toString());
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -158,6 +153,8 @@ public class TestCompile extends HttpServlet {
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (RuntimeException e) {
+			response.getWriter().append(e.getMessage());
 		}
 	}
 
